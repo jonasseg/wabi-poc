3a51902158d63cea3b28bfb9110ab9b8dbe6c411
Jonas Isaias Segura Javier <jisegura@atsistemas.com>
2022-06-19 22:08:04 +0200

    initial commit

---
+++ apps/wabi-poc-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wabi-poc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'wabi-poc-app';
+}
